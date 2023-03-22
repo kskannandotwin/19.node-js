09a4218e24d674e9f6fc578ed14065c0172a5f67
kskannandotwin <kskannan.win@gmail.com>
2023-03-22 06:44:04 +0530

    22.03.2023
    284. built in modules http-3 has completed

--- built-in-modules-http/app.js
+++ built-in-modules-http/app.js
@@ -1,12 +1,11 @@
 const http = require("http");
 const url = require("url");
 
-// connection settings
-// a port is an end point of communication
+//connection settings
+//a port is an end point of communication
 const port = 3000;
-
-// hostname: IP which is associated with any device connected to a computer network
-const hostname = "127.0.0.1"; // localhost
+//hostname: IP which is associated with any device connected to a computer network
+const hostname = "127.0.0.1"; //localhost
 
 const cars = [
   {
@@ -35,7 +34,7 @@ const cars = [
   },
 ];
 
-// callback function to be executed when a user makes a request to our server
+//callback function to be executed when a user makes a request to our server
 const respond = (request, response) => {
   if (request.url !== "/favicon.ico") {
     console.log(request.url);
@@ -43,33 +42,55 @@ const respond = (request, response) => {
     console.log(query);
     const pathname = url.parse(request.url, true).pathname;
     console.log(pathname);
+
+    //response.setHeader(header name, value)
+    response.setHeader("Content-Type", "text/plain");
+    //writeHead(status code, {headers})
+    response.writeHead(200, { "Content-Type": "text/html" });
+    //response.write sends the body of the response
+    response.write(`<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width">
+<title>Node</title>
+</head>
+<body>`);
+    response.write("<p>Node is fun!</p>");
+    //        console.log(car.make.toLowerCase());
+    const check = (car) =>
+      (query.make === undefined ||
+        query.make.toLowerCase() === car.make.toLowerCase()) &&
+      (query.model === undefined ||
+        query.model.toLowerCase() === car.model.toLowerCase()) &&
+      (query.year === undefined || query.year === car.year) &&
+      (query.transmission === undefined ||
+        query.transmission.toLowerCase() === car.transmission.toLowerCase()) &&
+      (query.minprice === undefined || parseInt(query.minprice) <= car.price) &&
+      (query.maxprice === undefined || parseInt(query.maxprice) >= car.price);
+    if (pathname === "/cars") {
+      cars.filter(check).forEach((car) => {
+        response.write(`
+<hr>
+<img src = '${car.url}' height='200' alt='car'>
+<p>Make: ${car.make}</p>
+<p>Model: ${car.model}</p>
+<p>Year: ${car.year}</p>
+<p>Transmission: ${car.transmission}</p>
+<p>Price: $${car.price}</p>
+`);
+      });
+      //make, model, year, transmission, minprice, maxprice
+    }
+    response.end(`</body>
+</html>`);
   }
-  // response.setHeader(header name, value)
-  response.setHeader("Content-Type", "text/plain");
-  // writeHead(status code, {headers})
-  response.writeHead(200, { "Content-Type": "text/html" });
-  // response.write sends the body of the response
-  response.write(`<!DOCTYPE html>
-  <html lang="en">
-  <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Node</title>
-  </head>
-  <body>`);
-  response.write(`<p>Node is fun!</p>`);
-  if (pathname === "/cars") {
-    car.filter().forEach();
-    // make, model, year, transmission, minprice, maxprice
-  }
-  response.end(`</body>
-      </html>`);
 };
 
-// create server
+//create a server
 const server = http.createServer(respond);
 
-// listen to user request
+//listen to user request
 server.listen(port, hostname, () => {
-  console.log(`listening on port ${port}, hostname: ${hostname}`);
+  console.log(`listening on port: ${port}, hostname: ${hostname}`);
 });
